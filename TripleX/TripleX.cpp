4496f183264d3f210ceb9a92348a7ad135bda696
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-06-07 00:07:02 +0200

    basic intro

--- TripleX/TripleX.cpp
+++ TripleX/TripleX.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
 
-int main(int argc, char* argv[])
+
+int main()
 {
+    std::cout << "You are the referee in the The UEFA European Football Championship Final.\nThe match ball is locked into a safe. But you forgot the code sheet at home!!\n";
+    std::cout << std::endl;        
+    std::cout << "The players are waiting, the fans are waiting, everyone is waiting for the final\n..\nCrack the safe by enter the correct codes to continue...";
+
+    int a = 4;
+    int b = 5;
+    int c = 7;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum;
+    std::cout << std::endl;
+    std::cout << product;
     
     return 0;
 }
